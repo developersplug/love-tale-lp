48fa8c96d11b2fec186b870a5b074afdfa58a012
developersplug <bssegcuf472@gmail.com>
2024-08-17 20:20:26 +0500

    changed

--- love-tale-lp/src/components/Availability/Availability.jsx
+++ love-tale-lp/src/components/Availability/Availability.jsx
@@ -10,7 +10,7 @@ const Availability = () => {
         <div className='flex-1 flex flex-col p-4 items-center justify-center'>
           <h2 className='font-medium md:text-[40px] text-[30px] tracking-tight md:tracking-normal mx-auto text-center'>Our <span className='underline underline-offset-2 decoration-pink-500'>30 Million</span> Downloads</h2>
           <p className='md:mx-[5%]  text-center md:text-[15px] text-[12px] my-3'>Download Lovetale online dating aap and you're always ready to check out profiles near you & chat with real commited singles ready for a real relationship.It puts at the power of eAmo in the palm of your hand for a faster dating experience.</p>
-          <div className='flex gap-3 my-4'>
+          <div className='flex gap-3 my-4 flex-wrap items-center justify-center'>
             <div className='flex items-center justify-center gap-1  bg-black rounded-md md:p-1 px-1 '>
               <FaApple className='md:text-2xl text-xl text-white' />
               <p className='md:text-[15px] text-[13px] leading-[1.2] text-white whitespace-nowrap'>
