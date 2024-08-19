9b9ed22d90d4af7e1b978c3748b7b36ffbcf2d02
developersplug <bssegcuf472@gmail.com>
2024-08-19 18:35:10 +0500

    footerlogo

--- love-tale-lp/src/components/Footer/Footer.jsx
+++ love-tale-lp/src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { FaFacebook,FaTwitter,FaInstagram } from "react-icons/fa";
+import LOGO02 from '../../assets/LOGO02.svg';
 
 
 const Footer = () => {
   return (
     <div className='w-[90%] mx-auto flex items-center h-[100px] '>
-        <h2  className='md:text-[25px] text-[20px] text-[#f47377] font-bold whitespace-nowrap'>Love Tale</h2>
+    <img src={LOGO02} alt="logo" className='w-[50px] h-[50px]' />
         <p className='mx-auto md:text-[22px] text-[12px]  text-center'>Copyright © 2024 Love Tale.All Rights Reserved.</p>
         <div className='text-black md:text-2xl text-xl flex items-center  text-center md:gap-2 gap-1'>
             <FaFacebook/>
